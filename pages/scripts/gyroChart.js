7bd2c432b23ced846c3dace80071052344a5e58e
Bartosz Chmielewski <nexerio7991@gmail.com>
2017-11-15 15:27:00 +0000

    Add files via upload

--- pages/scripts/gyroChart.js
+++ pages/scripts/gyroChart.js
@@ -7,91 +7,68 @@ socket.on('connect', function (){
       var elmarr=msg.topic.split("/");
       var elm=elmarr[3];
 //console.log(elmarr);
+      if( elmarr.indexOf('Accelerometer' && 'X-Axis') >= 0){//X-Axis queue
+        var sendData = msg.payload;
+        printText(sendData); //Publish data to the textArea
+      };
+      if( elmarr.indexOf('Accelerometer' && 'Y-Axis') >= 0){//X-Axis queue
+        var sendData = msg.payload;
+        printText2(sendData); //Publish data to the textArea
+      };
       if( elmarr.indexOf('Accelerometer' && 'Z-Axis') >= 0){//X-Axis queue
-        var sendData = "X-Axis: " + msg.payload;
-        printText('txtArea',elm,sendData); //Publish data to the textArea
-        Xaxis('Area1',elm,sendData);
+        var sendData = msg.payload;
+        printText3(sendData); //Publish data to the textArea
       };
 
-      if( elmarr.indexOf('Accelerometer' && 'Z-Axis') >= 0){//Y-Axis queue
-        var sendData2 = "Y-Axis: " + msg.payload;
-        printText('txtArea',elm,sendData2); //Publish data to the textArea
-        Yaxis('Area2',elm,sendData2);
-      };
 
-      if( elmarr.indexOf('Accelerometer' && 'Z-Axis') >= 0){//Z-Axis queue
-        var sendData3 = "Z-Axis: " + msg.payload;
-        printText('txtArea',elm,sendData3); //Publish data to the textArea
-        Zaxis('Area3',elm,sendData3);
+      if( elmarr.indexOf('Gyroscope' && 'X-Axis') >= 0){//X-Axis queue
+        var sendData = msg.payload;
+        printText4(sendData); //Publish data to the textArea
       };
-
+      if( elmarr.indexOf('Gyroscope' && 'Y-Axis') >= 0){//X-Axis queue
+        var sendData = msg.payload;
+        printText5(sendData); //Publish data to the textArea
+      };
       if( elmarr.indexOf('Gyroscope' && 'Z-Axis') >= 0){//X-Axis queue
-        var sendData4 = "X-Axis: " + msg.payload;
-        printText2('txtArea2',elm,sendData4); //Publish data to the textArea
-        gXaxis('Area4',elm,sendData4);
+        var sendData = msg.payload;
+        printText6(sendData); //Publish data to the textArea
       };
 
-      if( elmarr.indexOf('Gyroscope' && 'Z-Axis') >= 0){//Y-Axis queue
-        var sendData5 = "Y-Axis: " + msg.payload;
-        printText2('txtArea2',elm,sendData5); //Publish data to the textArea
-        gYaxis('Area5',elm,sendData5);
-      };
-
-      if( elmarr.indexOf('Gyroscope' && 'Z-Axis') >= 0){//Z-Axis queue
-        var sendData6 = "Z-Axis: " + msg.payload;
-        printText2('txtArea2',elm,sendData6); //Publish data to the textArea
-        gZaxis('Area6',elm,sendData6);
-      };
 
     });//Subscribe to the queue
     socket.emit('subscribe',{topic:'SmartHive/Accelerometer/#'});
     socket.emit('subscribe',{topic:'SmartHive/Gyroscope/#'});
 });
 //-----------------------Print to Text Area-------------------------------------
-function printText(chatID,ValueElm,PayloadValue){
-  $('#'+chatID).append("\n" + PayloadValue);
-  $('#'+chatID).scrollTop($('#'+chatID)[0].scrollHeight);
-  $('#'.concat(ValueElm)).html(PayloadValue);
+function printText(value2){
+  var secondValue = value2;
+  var data = secondValue;
+  document.getElementById("accX").firstChild.nodeValue = data;
+};
+function printText2(value2){
+  var secondValue = value2;
+  var data = secondValue;
+  document.getElementById("accY").firstChild.nodeValue = data;
+};
+function printText3(value2){
+  var secondValue = value2;
+  var data = secondValue;
+  document.getElementById("accZ").firstChild.nodeValue = data;
 };
 
-function printText2(chatID,ValueElm,PayloadValue){
-  $('#'+chatID).append("\n" + PayloadValue);
-  $('#'+chatID).scrollTop($('#'+chatID)[0].scrollHeight);
-  $('#'.concat(ValueElm)).html(PayloadValue);
-};
 
-function Xaxis(Area,ValueElm,PayloadValue){
-  $('#'+Area).append("\n" + PayloadValue);
-  $('#'+Area).scrollTop($('#'+Area)[0].scrollHeight);
-  $('#'.concat(ValueElm)).html(PayloadValue);
+function printText4(value2){
+  var secondValue = value2;
+  var data = secondValue;
+  document.getElementById("gyrX").firstChild.nodeValue = data;
 };
-
-function Yaxis(Area2,ValueElm,PayloadValue){
-  $('#'+Area2).append("\n" + PayloadValue);
-  $('#'+Area2).scrollTop($('#'+Area2)[0].scrollHeight);
-  $('#'.concat(ValueElm)).html(PayloadValue);
+function printText5(value2){
+  var secondValue = value2;
+  var data = secondValue;
+  document.getElementById("gyrY").firstChild.nodeValue = data;
 };
-
-function Zaxis(Area3,ValueElm,PayloadValue){
-  $('#'+Area3).append("\n" + PayloadValue);
-  $('#'+Area3).scrollTop($('#'+Area3)[0].scrollHeight);
-  $('#'.concat(ValueElm)).html(PayloadValue);
-};
-
-function gXaxis(Area4,ValueElm,PayloadValue){
-  $('#'+Area4).append("\n" + PayloadValue);
-  $('#'+Area4).scrollTop($('#'+Area4)[0].scrollHeight);
-  $('#'.concat(ValueElm)).html(PayloadValue);
-};
-
-function gYaxis(Area5,ValueElm,PayloadValue){
-  $('#'+Area5).append("\n" + PayloadValue);
-  $('#'+Area5).scrollTop($('#'+Area5)[0].scrollHeight);
-  $('#'.concat(ValueElm)).html(PayloadValue);
-};
-
-function gZaxis(Area6,ValueElm,PayloadValue){
-  $('#'+Area6).append("\n" + PayloadValue);
-  $('#'+Area6).scrollTop($('#'+Area6)[0].scrollHeight);
-  $('#'.concat(ValueElm)).html(PayloadValue);
+function printText6(value2){
+  var secondValue = value2;
+  var data = secondValue;
+  document.getElementById("gyrZ").firstChild.nodeValue = data;
 };
--- pages/scripts/weight.js
+++ pages/scripts/weight.js
@@ -7,13 +7,26 @@ socket.on('connect', function (){
       var elmarr=msg.topic.split("/");
       var elm=elmarr[3];
 //console.log(elmarr);
-      if( elmarr.indexOf('Weight') >= 0){//X-Axis queue
-        var sendData = msg.payload;
-        printText('txtArea',elm,sendData); //Publish data to the textArea
-        Xaxis('Area1',elm,sendData);
-      };
 
-    });//Subscribe to the queue
+      var d = new Date();//Get Date/Time for the times array
+      var n = d.getHours()+ ":" + d.getMinutes()+ ":" + d.getSeconds();
+
+      if( elmarr.indexOf('Weight') >= 0){
+        str = msg.payload;
+        var trim = str.replace(/(?:\r\n|\r|\n)/g, '');
+        var sendData = n + "-Weight:" + trim + " KG";
+        printText('txtArea',elm,sendData); //Publish data to the textArea
+
+        var test = msg.payload;
+        var res = test.split(".");
+
+        var res1 = res[0];
+        var res2 = res[1];
+
+        updateWeight(res1,res2);
+      }
+
+  });//Subscribe to the queue
     socket.emit('subscribe',{topic:'SmartHive/Weight'});
 });
 //-----------------------Print to Text Area-------------------------------------
@@ -22,3 +35,18 @@ function printText(chatID,ValueElm,PayloadValue){
   $('#'+chatID).scrollTop($('#'+chatID)[0].scrollHeight);
   $('#'.concat(ValueElm)).html(PayloadValue);
 };
+
+jQuery(function($) {
+  setInterval(function() {
+    var date = new Date(),
+        time = date.toLocaleTimeString();
+    $(".clock").html(time);
+  }, 1000);
+});
+
+function updateWeight(value1,value2) {
+  var firstValue = value1;
+  var secondValue = value2;
+  var data = firstValue + "." + secondValue +" " + "kg";
+  document.getElementById("glock").firstChild.nodeValue = data;
+}
